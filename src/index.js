4eff9ae6a0f2764472860358ec427d8b8b8a82bd
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-05-25 16:23:12 -0700

    Altering states in the game

--- src/index.js
+++ src/index.js
@@ -2,39 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/*
-class Square extends React.Component {
-
-    constructor(props) {
-        super(props);
-        /*
-        Use the below line to bind 'this' to the function if clickBehaviour() is defined 
-        as a normal (non-arrow) function. This is because util arrow functions,
-        every new function defined its own 'this' value.
-
-        From ES7+ (ES2016), this can also be written as (syntactic sugar):
-        this.clickBehaviour = ::this.clickBehaviour
-
-        The below line is currently redundant as 'clickBehaviour' is an arrow function.
-        /
-
-       this.clickBehaviour = this.clickBehaviour.bind(this);
-    }
-
-    render() {
-        return (
-            <button className="square" onClick={this.clickBehaviour}>
-                {this.props.value}
-            </button>
-        );
-    }
-
-    clickBehaviour = () => {
-        this.props.onClick();
-    }
-}
-*/
-
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
@@ -47,14 +14,18 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -64,7 +35,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
@@ -111,4 +82,38 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
-  
+
+/**
+ * This Square component was refactored to be a light-weight 'function component'.
+ * Retaining the original component for recall value during learing.
+ *
+ * class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // Use the below line to bind 'this' to the function if clickBehaviour() is defined
+        // as a normal (non-arrow) function. This is because util arrow functions,
+        // every new function defined its own 'this' value.
+
+        // From ES7+ (ES2016), this can also be written as (syntactic sugar):
+        // this.clickBehaviour = ::this.clickBehaviour
+
+        // The below line is currently redundant as 'clickBehaviour' is an arrow function.
+
+       this.clickBehaviour = this.clickBehaviour.bind(this);
+    }
+
+    render() {
+        return (
+            <button className="square" onClick={this.clickBehaviour}>
+                {this.props.value}
+            </button>
+        );
+    }
+
+    clickBehaviour = () => {
+        this.props.onClick();
+    }
+}
+*/
